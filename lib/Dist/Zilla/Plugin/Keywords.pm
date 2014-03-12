1b15dd3177e69b3e797ac21d87949ee374e185c0
Karen Etheridge <ether@cpan.org>
2014-03-12 10:55:05 +0100

    code, docs, tests

--- lib/Dist/Zilla/Plugin/Keywords.pm
+++ lib/Dist/Zilla/Plugin/Keywords.pm
@@ -1,31 +1,67 @@
 use strict;
 use warnings;
 package Dist::Zilla::Plugin::Keywords;
-# ABSTRACT: ...
+# ABSTRACT: add keywords to metadata in your distribution
 # vim: set ts=8 sw=4 tw=78 et :
 
+use Moose;
+with 'Dist::Zilla::Role::MetaProvider';
+use Moose::Util::TypeConstraints;
+use MooseX::Types::Moose 'ArrayRef';
+use MooseX::Types::Common::String 'NonEmptySimpleStr';
+use namespace::autoclean;
+
+my $word = subtype NonEmptySimpleStr,
+    where { !/\s/ };
+
+my $wordlist = subtype ArrayRef[$word];
+coerce $wordlist, from ArrayRef[NonEmptySimpleStr],
+    via { [ map { split /\s+/, $_ } @$_ ] };
 
 
-1;
+sub mvp_aliases { +{ keyword => 'keywords' } }
+sub mvp_multivalue_args { qw(keywords) }
+
+has keywords => (
+    is => 'ro', isa => $wordlist,
+    coerce => 1,
+    default => sub { [] },
+);
+
+sub metadata
+{
+    my $self = shift;
+    return { keywords => $self->keywords };
+}
+
+__PACKAGE__->meta->make_immutable;
 __END__
 
 =pod
 
 =head1 SYNOPSIS
 
-    use Dist::Zilla::Plugin::Keywords;
+In your F<dist.ini>:
 
-    ...
+    [Keywords]
+    keyword = plugin
+    keyword = tool
+    keywords = development Dist::Zilla
 
 =head1 DESCRIPTION
 
-...
+This plugin adds metadata to your distribution under the C<keywords> field.
+The L<CPAN meta specification|https://metacpan.org/pod/CPAN::Meta::Spec#keywords>
+defines this field as:
 
-=head1 FUNCTIONS/METHODS
+    A List of keywords that describe this distribution. Keywords must not include whitespace.
 
-=head2 C<foo>
+=head1 CONFIGURATION OPTIONS
 
-...
+=head2 C<keyword>, C<keywords>
+
+One or more words to be added as keywords. Can be repeated more than once.
+Strings are broken up by whitespace and added as separate words.
 
 =head1 SUPPORT
 
@@ -35,15 +71,11 @@ Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Di
 (or L<bug-Dist-Zilla-Plugin-Keywords@rt.cpan.org|mailto:bug-Dist-Zilla-Plugin-Keywords@rt.cpan.org>).
 I am also usually active on irc, as 'ether' at C<irc.perl.org>.
 
-=head1 ACKNOWLEDGEMENTS
-
-...
-
 =head1 SEE ALSO
 
 =begin :list
 
-* L<foo>
+* L<https://metacpan.org/pod/CPAN::Meta::Spec#keywords>
 
 =end :list
 
--- t/01-basic.t
+++ t/01-basic.t
@@ -3,8 +3,66 @@ use warnings FATAL => 'all';
 
 use Test::More;
 use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
-use Dist::Zilla::Plugin::Keywords;
+use Test::Deep;
+use Test::Deep::JSON;
+use Test::DZil;
+use Path::Tiny;
 
-fail('this test is TODO!');
+my $preamble = <<'PREAMBLE';
+name = DZT-Sample
+abstract = Sample DZ Dist
+version  = 0.001
+author   = E. Xavier Ample <example@example.org>
+license  = Perl_5
+copyright_holder = E. Xavier Ample
+
+PREAMBLE
+
+foreach my $dist_ini (
+    simple_ini(
+        [ MetaJSON => ],
+        [ Keywords => { keywords => [ qw(foo bar baz) ] } ],
+    ),
+    $preamble . <<'INI',
+[MetaJSON]
+[Keywords]
+keyword = foo
+keyword = bar
+keyword = baz
+INI
+    $preamble . <<'INI',
+[MetaJSON]
+[Keywords]
+keywords = foo bar baz
+INI
+    $preamble . <<'INI',
+[MetaJSON]
+[Keywords]
+keywords = foo bar
+keyword = baz
+INI
+)
+{
+    my $tzil = Builder->from_config(
+        { dist_root => 't/does_not_exist' },
+        {
+            add_files => {
+                path(qw(source dist.ini)) => $dist_ini,
+            },
+        },
+    );
+
+    $tzil->build;
+
+    my $json = $tzil->slurp_file('build/META.json');
+    cmp_deeply(
+        $json,
+        json(superhashof({
+            dynamic_config => 0,
+            keywords => [ qw(foo bar baz) ],
+        })),
+        'metadata is correct',
+    );
+}
 
 done_testing;

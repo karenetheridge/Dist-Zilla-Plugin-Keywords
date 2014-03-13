730a7f6a11dae13947823137f6c316a6de0e63f4
Karen Etheridge <ether@cpan.org>
2014-03-13 13:33:13 +0100

    now also fall back to scanning for a # KEYWORDS: comment

--- lib/Dist/Zilla/Plugin/Keywords.pm
+++ lib/Dist/Zilla/Plugin/Keywords.pm
@@ -2,10 +2,11 @@ use strict;
 use warnings;
 package Dist::Zilla::Plugin::Keywords;
 # ABSTRACT: add keywords to metadata in your distribution
+# KEYWORDS: plugin distribution metadata cpan-meta keywords
 # vim: set ts=8 sw=4 tw=78 et :
 
 use Moose;
-with 'Dist::Zilla::Role::MetaProvider';
+with 'Dist::Zilla::Role::MetaProvider', 'Dist::Zilla::Role::PPI';
 use Moose::Util::TypeConstraints;
 use MooseX::Types::Moose 'ArrayRef';
 use MooseX::Types::Common::String 'NonEmptySimpleStr';
@@ -25,13 +26,29 @@ sub mvp_multivalue_args { qw(keywords) }
 has keywords => (
     is => 'ro', isa => $wordlist,
     coerce => 1,
-    default => sub { [] },
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        my @keywords = $self->keywords_from_file($self->zilla->main_module);
+        \@keywords;
+    },
 );
 
 sub metadata
 {
     my $self = shift;
     return { keywords => $self->keywords };
+}
+
+sub keywords_from_file
+{
+    my ($self, $file) = @_;
+
+    my $document = $self->ppi_document_for_file($file);
+    my $node = $document->find_first('PPI::Token::Comment');
+    my @keywords = $node->content =~ m/^\s*#+\s*KEYWORDS:\s*(.+)$/mg;
+    $self->log('found keyword string in main module: ' . $_) foreach @keywords;
+    return map { split /\s+/ } @keywords;
 }
 
 __PACKAGE__->meta->make_immutable;
@@ -48,6 +65,14 @@ In your F<dist.ini>:
     keyword = tool
     keywords = development Dist::Zilla
 
+Or, in your F<dist.ini>:
+
+    [Keywords]
+
+And in your main module:
+
+    # KEYWORDS: plugin development tool
+
 =head1 DESCRIPTION
 
 This plugin adds metadata to your distribution under the C<keywords> field.
@@ -56,7 +81,7 @@ defines this field as:
 
     A List of keywords that describe this distribution. Keywords must not include whitespace.
 
-=for Pod::Coverage metadata mvp_aliases mvp_multivalue_args
+=for Pod::Coverage metadata mvp_aliases mvp_multivalue_args keywords_from_file
 
 =head1 CONFIGURATION OPTIONS
 
@@ -64,6 +89,9 @@ defines this field as:
 
 One or more words to be added as keywords. Can be repeated more than once.
 Strings are broken up by whitespace and added as separate words.
+
+If no configuration is provided, the main module of your distribution is
+scanned for the I<first> C<# KEYWORDS:> comment.
 
 =head1 SUPPORT
 
---
+++ t/02-keywords-from-file.t
@@ -0,0 +1,51 @@
+use strict;
+use warnings FATAL => 'all';
+
+use Test::More;
+use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
+use Test::Deep;
+use Test::Deep::JSON;
+use Test::DZil;
+use Path::Tiny;
+
+my $tzil = Builder->from_config(
+    { dist_root => 't/does_not_exist' },
+    {
+        add_files => {
+            path(qw(source dist.ini)) => simple_ini(
+                [ GatherDir => ],
+                [ MetaJSON => ],
+                [ Keywords => ],
+            ),
+            path(qw(source lib Foo.pm)) => <<MODULE,
+package Foo;
+# KEYWORDS: foo bar baz
+# KEYWORDS: and more here, to be ignored
+1;
+=pod
+
+=head1 SYNOPSIS
+
+    # KEYWORDS: do not find these
+
+=cut
+MODULE
+        },
+    },
+);
+
+$tzil->chrome->logger->set_debug(1);
+$tzil->build;
+
+my $json = $tzil->slurp_file('build/META.json');
+cmp_deeply(
+    $json,
+    json(superhashof({
+        dynamic_config => 0,
+        keywords => [ qw(foo bar baz) ],
+    })),
+    'metadata is correct',
+) or diag 'saw messages:' . join("\n", @{ $tzil->log_messages });
+
+
+done_testing;
